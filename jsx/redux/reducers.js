f0525e640a9a45349957960854b06db21caeba38
work-react <75259775+work-react@users.noreply.github.com>
2021-01-12 22:36:47 +0300

    update add_operation

--- jsx/redux/reducers.js
+++ jsx/redux/reducers.js
@@ -1,6 +1,6 @@
 export function rootReducer(state = initialState, action){
-	if(action.type="Add_operator" && action.value) {
-		state.all.push(action.value.value + /\(/.test(action.value.value)?')':'' + action.value.c)
+	if(action.type="Add_operation" && action.value) {
+		state.all.push(action.value.value + (/\(/.test(action.value.value)?')':'') + '=' + action.value.c)
 		return state;
 	}
 	return state;
